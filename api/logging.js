a47472d4a60b056ecbbd492b396d76db007d5c66
IgorCroitoru <croitoru-2013@mail.ru>
2026-05-01 16:12:57 +0300

    mods

--- api/logging.js
+++ api/logging.js
@@ -1,17 +1,17 @@
-// const { createLogger } = require("@market-bot-admin/logging");
+const { createLogger } = require("@market-bot-admin/logging");
 
-// function createApiLogger(context) {
-//   return createLogger({
-//     service: "api",
-//     environment: "dev",
-//     level: "debug",
-//     context: {
-//       invocationId: context?.invocationId,
-//       functionName: context?.functionName
-//     }
-//   });
-// }
+function createApiLogger(context) {
+  return createLogger({
+    service: "api",
+    environment: "dev",
+    level: "debug",
+    context: {
+      invocationId: context?.invocationId,
+      functionName: context?.functionName
+    }
+  });
+}
 
-// module.exports = {
-//   createApiLogger
-// };
+module.exports = {
+  createApiLogger
+};
